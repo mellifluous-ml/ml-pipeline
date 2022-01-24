b5a28f6590e02e48cd0c2027f9d1d521ef4905e8
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.co>
2022-01-24 04:14:04 +0200

    Created basic app with home page

--- datafetcher/main.py
+++ datafetcher/main.py
@@ -1,7 +1,14 @@
+import uvicorn
 from fastapi import FastAPI
-import os
 
 app = FastAPI()
 
-print(f"{os.getenv('SERVICE_NAME')} service.")
-print(list(os.listdir(".")))
+
+@app.get("/")
+@app.get("/home")
+def home():
+    return {"message": "home"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True, port=5000)
